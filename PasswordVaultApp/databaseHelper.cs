9119777facbd35617c43f3a738eda613594f9ea6
N1K0l0Z-KAKAUR1DZ3 <you@example.com>
2026-03-17 04:55:11 +0400

    Update .gitignore to exclude project plan, add user project file for database management and add a missing file

--- PasswordVaultApp/databaseHelper.cs
+++ PasswordVaultApp/databaseHelper.cs
@@ -3,9 +3,19 @@ using Microsoft.Data.SqlClient;
 
 namespace PasswordVaultApp
 {
-    public static class DatabaseManager
+      public static class DatabaseManager
     {
-        private static SqlConnection connection = new SqlConnection("Server=DESKTOP-LS4I652;Database=PasswordVaultDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        private static string connectionString = "Server=DESKTOP-LS4I652;Database=PasswordVaultDB;Trusted_Connection=True;TrustServerCertificate=True;";
 
+        private delegate void dataBaseConfigurationLogic (SqlConnection sqlConnection);
+
+        private static void TalkToDB(dataBaseConfigurationLogic dbModificationLogic)
+        {
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                dbModificationLogic(connection);
+            }
+        }
     }
 }
